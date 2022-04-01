13ffa88d9809ace53faf5c5310e6accbb64ce0d0
IGarcillano <igarcillano@moore.edu>
2022-03-31 23:46:26 -0400

    Player Movement
    Basic Player movement coding added, but instead of a joystick, it is the whole screen, but I found a new tutorial to try, so the coding will be changed.
    
    The bear's idle animation plays on scene, but does not do anything else.

---
+++ IzaKP_Project/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //This script allows user to move player left and right on touch screen
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    private float moveSpeed;
+
+   private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (touch.position.x < Screen.width / 2)
+                        rb.velocity = new Vector2(- moveSpeed, 0f);
+
+                    if (touch.position.x > Screen.width / 2)
+                        rb.velocity = new Vector2(moveSpeed, 0f);
+                    break;
+
+                case TouchPhase.Ended:
+                    rb.velocity = new Vector2(0f, 0f);
+                    break;
+            }
+        }
+    }
+}
